f6af5e8e3460e19b49fd9420e0dec1b227cb5b96
Brandon Lavigne <Brandon_Lavigne2@intuit.com>
2018-09-15 13:52:16 -0700

    xhr request for data, and highlight

--- dom.js
+++ dom.js
@@ -1,3 +1,31 @@
 (function() {
-  console.log('yo!');
+  const redAlert = [
+    'plastic straws',
+    'Brandon',
+  ];
+
+  // http://2018zoohackathon.ajzane.com/wp-json/wp/v2/posts/
+
+  var myImage = document.querySelector('img');
+
+  fetch('http://2018zoohackathon.ajzane.com/wp-json/wp/v2/posts/', {
+        method: 'get'
+    })
+    .then(response => response.json())
+    .then(jsonData => console.log(jsonData))
+    .catch(err => {
+            console.log(err);
+          });
+
+
+  var context = document.querySelector("body"); // requires an element with class "context" to exist
+  var options = {
+    end: function(count) {
+      console.log(count);
+    }
+  };
+  var instance = new Mark(context, options);
+  instance.markRegExp(/Karazan/gmi);
+  console.log(instance.markRanges.length);
+  
 })();
