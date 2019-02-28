49d4466e844ee8a6b19e2ef90ff44a3f9fe4bac2
Brandon Lavigne <brandon@caavadesign.com>
2019-02-27 21:52:01 -0800

    add prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-const pjson = require('./package.json');
+const pjson = require("./package.json");
 
-const PAGES_PATH = './src/pages';
+const PAGES_PATH = "./src/pages";
 
 function generateHtmlPlugins(items) {
-  return items.map(name => new HtmlPlugin(
-    {
-      filename: `./${name}.html`,
-      chunks: [name],
-      template: './src/pages/template.html',
-    },
-  ));
+  return items.map(
+    name =>
+      new HtmlPlugin({
+        filename: `./${name}.html`,
+        chunks: [name],
+        template: "./src/pages/template.html"
+      })
+  );
 }
 
 function modifyManifest(buffer) {
@@ -36,16 +37,16 @@ const config = {
     content: `${PAGES_PATH}/content`,
     options: `${PAGES_PATH}/options`,
     popup: `${PAGES_PATH}/popup`,
-    polyfills: `${PAGES_PATH}/polyfills`,
+    polyfills: `${PAGES_PATH}/polyfills`
   },
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
+    react: "React",
+    "react-dom": "ReactDOM"
   },
   output: {
-    path: path.resolve('dist/pages'),
-    filename: '[name].js',
+    path: path.resolve("dist/pages"),
+    filename: "[name].js"
   },
   module: {
     rules: [
@@ -53,65 +54,65 @@ const config = {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: "babel-loader"
+        }
       },
       {
         test: /\.jpe?g$|\.gif$|\.png$|\.ttf$|\.eot$|\.svg$/,
-        use: 'file-loader?name=[name].[ext]?[hash]',
+        use: "file-loader?name=[name].[ext]?[hash]"
       },
       {
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
-      },
-    ],
+        loaders: ["style-loader", "css-loader"]
+      }
+    ]
   },
   plugins: [
     new BundleAnalyzerPlugin({ analyzerPort: 8777 }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: '[name].css',
-      chunkFilename: '[id].css',
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     }),
-    new CopyPlugin(
-      [
-        {
-          from: 'src',
-          to: path.resolve('dist'),
-          ignore: ['actions', 'components', 'redux', 'reducers', 'utilities', 'pages/**/*', 'manifest.json'],
-        },
-        {
-          from: './src/manifest.json',
-          to: path.resolve('dist'),
-          transform(content) {
-            return modifyManifest(content);
-          },
-        },
-      ],
-    ),
-    ...generateHtmlPlugins(
-      [
-        'background',
-        'options',
-        'popup',
-      ],
-    ),
-  ],
+    new CopyPlugin([
+      {
+        from: "src",
+        to: path.resolve("dist"),
+        ignore: [
+          "actions",
+          "components",
+          "redux",
+          "reducers",
+          "utilities",
+          "pages/**/*",
+          "manifest.json"
+        ]
+      },
+      {
+        from: "./src/manifest.json",
+        to: path.resolve("dist"),
+        transform(content) {
+          return modifyManifest(content);
+        }
+      }
+    ]),
+    ...generateHtmlPlugins(["background", "options", "popup"])
+  ]
 };
 
-if (!process.argv.includes('development')) {
+if (!process.argv.includes("development")) {
   config.optimization = {
     minimizer: [
       new TerserPlugin({
         sourceMap: true,
         terserOptions: {
           compress: {
-            drop_console: true,
-          },
-        },
-      }),
-    ],
+            drop_console: true
+          }
+        }
+      })
+    ]
   };
 }
 
